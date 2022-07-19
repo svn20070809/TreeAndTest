bd9ede1a06c44295f23f13fb8b01e6ebb9b20839
SVN <svn20070809@gmail.com>
2022-07-19 23:03:46 +0300

    Обновил в VS committed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-     //   test_1();
       // Добавил комментарий
       var x=1;
       var y=2;
         y += x;
+        x+=y;
+        test_1();
+     
     }
     // https://javascopes.com/java-binary-tree-b6863141/
 
     static void test_1() {
         // write your code here
+        System.out.println("Hello");
         Tree bt = new Tree();
         bt.add(6);
         bt.add(4);
